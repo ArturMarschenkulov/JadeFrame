be57ba54be133a074fff97912fd89e429e8d7ca1
ArturMarschenkulov <ArturMarschals@web.de>
2022-06-06 04:42:49 +0200

    autoformated the file and some minor changes

--- JadeFrame/src/JadeFrame/graphics/opengl/opengl_renderer.h
+++ JadeFrame/src/JadeFrame/graphics/opengl/opengl_renderer.h
@@ -25,81 +25,78 @@
 namespace JadeFrame {
 
 enum class PRIMITIVE_TYPE {
-	TRIANGLES = GL_TRIANGLES,
-	LINES = GL_LINES,
-	POINTS = GL_POINTS,
+    TRIANGLES = GL_TRIANGLES,
+    LINES = GL_LINES,
+    POINTS = GL_POINTS,
 };
 
 struct OpenGL_Material {
-	const OpenGL_Texture* m_texture = nullptr;
-	OpenGL_Shader* m_shader = nullptr;
+    const OpenGL_Texture* m_texture = nullptr;
+    OpenGL_Shader*        m_shader = nullptr;
 };
 
 
 
 class Object {
 public:
-	VertexData* m_vertex_data;
-	VertexFormat m_vertex_format;
-	MaterialHandle* m_material_handle;
-	Matrix4x4 m_transform;
-	mutable GPUDataMeshHandle m_GPU_mesh_data;
+    VertexData*               m_vertex_data;
+    VertexFormat              m_vertex_format;
+    MaterialHandle*           m_material_handle;
+    Matrix4x4                 m_transform;
+    mutable GPUDataMeshHandle m_GPU_mesh_data;
 };
 
 
 struct OpenGL_RenderCommand {
-	const Matrix4x4* transform = nullptr;
-	const VertexData* vertex_data = nullptr;
-	MaterialHandle* material_handle = nullptr;
-	const GPUDataMeshHandle* m_GPU_mesh_data = nullptr;
+    const Matrix4x4*         transform = nullptr;
+    const VertexData*        vertex_data = nullptr;
+    MaterialHandle*          material_handle = nullptr;
+    const GPUDataMeshHandle* m_GPU_mesh_data = nullptr;
 };
 class OpenGL_CommandBuffer {
 public:
-	//auto push(const Mesh& mesh, const OpenGL_Material& material, const Matrix4x4& tranform, const OpenGL_VertexArray& vertex_array) -> void;
-	std::vector<OpenGL_RenderCommand> m_render_commands;
+    // auto push(const Mesh& mesh, const OpenGL_Material& material, const Matrix4x4& tranform, const OpenGL_VertexArray&
+    // vertex_array) -> void;
+    std::vector<OpenGL_RenderCommand> m_render_commands;
 };
 
 
 class OpenGL_Renderer : public IRenderer {
 public:
+    OpenGL_Renderer(const Window& window);
 
-	OpenGL_Renderer(const Window& window);
+    virtual auto present() -> void override;
+    virtual auto clear_background() -> void override;
+    virtual auto render(const Matrix4x4& view_projection) -> void override;
 
-	virtual auto present() -> void override;
-	virtual auto clear_background() -> void override;
-	virtual auto render(const Matrix4x4& view_projection) -> void override;
-
-	virtual auto submit(const Object& obj) -> void override;
+    virtual auto submit(const Object& obj) -> void override;
 
     virtual auto set_clear_color(const RGBAColor& color) -> void override;
     virtual auto set_viewport(u32 x, u32 y, u32 width, u32 height) const -> void override;
 
 
-	virtual auto take_screenshot(const char* filename) -> void override;
+    virtual auto take_screenshot(const char* filename) -> void override;
 
 
 
 
 private:
-	auto render_mesh(const OpenGL_GPUMeshData* buffer_data, const VertexData* vertex_data) const -> void;
+    auto render_mesh(const OpenGL_GPUMeshData* buffer_data, const VertexData* vertex_data) const -> void;
 
 private:
-	OpenGL_Context m_context;
-	mutable std::deque<OpenGL_RenderCommand> m_render_commands;
+    OpenGL_Context                           m_context;
+    mutable std::deque<OpenGL_RenderCommand> m_render_commands;
 
-	struct FB {
-		Object m_fb;
-		OGLW_Texture<GL_TEXTURE_2D> m_framebuffer_texture;
-		OGLW_Renderbuffer m_framebuffer_renderbuffer;
-		OGLW_Framebuffer m_framebuffer;
-		OpenGL_GPUMeshData* m_framebuffer_rect;
-		ShaderHandle* m_shader_handle_fb;
-	} fb;
-
-
-	
+    struct FB {
+        Object                      m_fb;
+        OGLW_Texture<GL_TEXTURE_2D> m_framebuffer_texture;
+        OGLW_Renderbuffer           m_framebuffer_renderbuffer;
+        OGLW_Framebuffer            m_framebuffer;
+        OpenGL_GPUMeshData*         m_framebuffer_rect;
+        ShaderHandle*               m_shader_handle_fb;
+    } fb;
 };
-struct RenderSystem {
+static_assert(is_renderer<OpenGL_Renderer>);
 
-};
-}
+struct RenderSystem {};
+} // namespace JadeFrame
