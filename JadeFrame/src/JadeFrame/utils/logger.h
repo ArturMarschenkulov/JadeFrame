9a60e9f36929808d680e5a6f49fb02cc1f770011
artur <ArturMarschals@web.de>
2022-09-16 23:41:53 +0200

    cleaned up and refacrored logger code

--- JadeFrame/src/JadeFrame/utils/logger.h
+++ JadeFrame/src/JadeFrame/utils/logger.h
@@ -34,14 +34,8 @@ public:
     };
     template<class... Types>
     static auto log(fmt::format_string<Types...> text, Types&&... args) -> void;
-    // template<class T>
-    // static auto log(const T& text) -> void;
     template<class... Types>
     static auto log(LEVEL level, fmt::format_string<Types...> text, Types&&... args) -> void;
-    // template<class T>
-    // static auto log(LEVEL level, const T& text) -> void;
-
-
 
     template<class... Types>
     static auto trace(fmt::format_string<Types...> text, Types&&... args) -> void;
@@ -56,18 +50,24 @@ public:
     template<class... Types>
     static auto critical(fmt::format_string<Types...> text, Types&&... args) -> void;
 
-    // template<class T>
-    // static auto trace(const T& text) -> void;
-    // template<class T>
-    // static auto debug(const T& text) -> void;
-    // template<class T>
-    // static auto info(const T& text) -> void;
-    // template<class T>
-    // static auto warn(const T& text) -> void;
-    // template<class T>
-    // static auto err(const T& text) -> void;
-    // template<class T>
-    // static auto critical(const T& text) -> void;
+
+    // template<class... Types>
+    // static auto log(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto log(LEVEL level, fmt::wformat_string<Types...> text, Types&&... args) -> void;
+
+    // template<class... Types>
+    // static auto trace(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto debug(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto info(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto warn(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto err(fmt::wformat_string<Types...> text, Types&&... args) -> void;
+    // template<class... Types>
+    // static auto critical(fmt::wformat_string<Types...> text, Types&&... args) -> void;
 
 
 
@@ -83,10 +83,6 @@ template<class... Types>
 auto Logger::log(fmt::format_string<Types...> text, Types&&... args) -> void {
     Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
 }
-// template<class T>
-// auto Logger::log(const T& text) -> void {
-//     Logger::log(LEVEL::WARN, text);
-// }
 
 template<class... Types>
 auto Logger::log(LEVEL level, fmt::format_string<Types...> text, Types&&... args) -> void {
@@ -100,10 +96,50 @@ auto Logger::log(LEVEL level, fmt::format_string<Types...> text, Types&&... args
         case LEVEL::CRITICAL: lvl = spdlog::level::critical; break;
         default: lvl = spdlog::level::off; break;
     }
+    // if (sizeof...(args) > 1) {
+    //     s_core->log(lvl, text, std::forward<Types>(args)...);
+    // } else {
+    //     s_core->log(lvl, text);
+    // }
     s_core->log(lvl, text, std::forward<Types>(args)...);
 }
-// template<class T>
-// auto Logger::log(LEVEL level, const T& text) -> void {
+
+template<class... Types>
+auto Logger::trace(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::TRACE, text, std::forward<Types>(args)...);
+}
+template<class... Types>
+auto Logger::debug(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::DEBUG, text, std::forward<Types>(args)...);
+}
+template<class... Types>
+auto Logger::info(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::INFO, text, std::forward<Types>(args)...);
+}
+template<class... Types>
+auto Logger::warn(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
+}
+template<class... Types>
+auto Logger::err(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::ERR, text, std::forward<Types>(args)...);
+}
+template<class... Types>
+auto Logger::critical(fmt::format_string<Types...> text, Types&&... args) -> void {
+    Logger::log(LEVEL::CRITICAL, text, std::forward<Types>(args)...);
+}
+
+
+
+// // Wide char version
+
+// template<class... Types>
+// auto Logger::log(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
+// }
+
+// template<class... Types>
+// auto Logger::log(LEVEL level, fmt::wformat_string<Types...> text, Types&&... args) -> void {
 //     spdlog::level::level_enum lvl;
 //     switch (level) {
 //         case LEVEL::TRACE: lvl = spdlog::level::trace; break;
@@ -114,71 +150,32 @@ auto Logger::log(LEVEL level, fmt::format_string<Types...> text, Types&&... args
 //         case LEVEL::CRITICAL: lvl = spdlog::level::critical; break;
 //         default: lvl = spdlog::level::off; break;
 //     }
-//     s_core->log(lvl, text);
+
+//     s_core->log(lvl, text, std::forward<Types>(args)...);
 // }
 
-
-
-
-template<class... Types>
-auto Logger::trace(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->trace(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::TRACE, text, std::forward<Types>(args)...);
-}
-template<class... Types>
-auto Logger::debug(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->debug(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::DEBUG, text, std::forward<Types>(args)...);
-}
-template<class... Types>
-auto Logger::info(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->info(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::INFO, text, std::forward<Types>(args)...);
-}
-template<class... Types>
-auto Logger::warn(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->warn(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
-}
-template<class... Types>
-auto Logger::err(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->error(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::ERR, text, std::forward<Types>(args)...);
-}
-template<class... Types>
-auto Logger::critical(fmt::format_string<Types...> text, Types&&... args) -> void {
-    // s_core->critical(text, std::forward<Types>(args)...);
-    Logger::log(LEVEL::CRITICAL, text, std::forward<Types>(args)...);
-}
-
-// template<class T>
-// auto Logger::trace(const T& text) -> void {
-//     // s_core->trace(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::TRACE, text);
+// template<class... Types>
+// auto Logger::trace(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::TRACE, text, std::forward<Types>(args)...);
 // }
-// template<class T>
-// auto Logger::debug(const T& text) -> void {
-//     // s_core->debug(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::DEBUG, text);
+// template<class... Types>
+// auto Logger::debug(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::DEBUG, text, std::forward<Types>(args)...);
 // }
-// template<class T>
-// auto Logger::info(const T& text) -> void {
-//     // s_core->info(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::INFO, text);
+// template<class... Types>
+// auto Logger::info(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::INFO, text, std::forward<Types>(args)...);
 // }
-// template<class T>
-// auto Logger::warn(const T& text) -> void {
-//     // s_core->warn(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::WARN, text);
+// template<class... Types>
+// auto Logger::warn(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
 // }
-// template<class T>
-// auto Logger::err(const T& text) -> void {
-//     // s_core->error(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::ERR, text);
+// template<class... Types>
+// auto Logger::err(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::ERR, text, std::forward<Types>(args)...);
 // }
-// template<class T>
-// auto Logger::critical(const T& text) -> void {
-//     // s_core->critical(text, std::forward<Types>(args)...);
-//     Logger::log(LEVEL::CRITICAL, text);
+// template<class... Types>
+// auto Logger::critical(fmt::wformat_string<Types...> text, Types&&... args) -> void {
+//     Logger::log(LEVEL::CRITICAL, text, std::forward<Types>(args)...);
 // }
 } // namespace JadeFrame
