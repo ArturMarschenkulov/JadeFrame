e89e44cefe3a109ead964458ddcbf3860724b391
artur <ArturMarschals@web.de>
2023-03-26 16:42:49 +0200

    added macros

--- JadeFrame/src/JadeFrame/macros.h
+++ JadeFrame/src/JadeFrame/macros.h
@@ -22,3 +22,52 @@
 #define JF_PRAGMA_DISABLE(x)
 #error("Unknown compiler")
 #endif
+
+// Define compiler macros
+#if defined(__clang__)
+#define JF_COMPILER_CLANG
+#elif defined(__GNUC__)
+#define JF_COMPILER_GCC
+#elif defined(_MSC_VER)
+#define JF_COMPILER_MSVC
+#else
+#error("Unknown compiler")
+#endif
+
+// Define platform/os macros
+#if defined(_WIN32)
+#define JF_PLATFORM_WINDOWS
+#elif defined(__linux__)
+#define JF_PLATFORM_LINUX
+#elif defined(__APPLE__)
+#define JF_PLATFORM_MACOS
+#else
+#error("Unknown platform")
+#endif
+
+// Define architecture macros
+#if defined(__x86_64__) || defined(_M_X64)
+#define JF_ARCH_X86_64
+#elif defined(__i386) || defined(_M_IX86)
+#define JF_ARCH_X86
+#else
+#error("Unknown architecture")
+#endif
+
+// Define debug/release macros
+#if defined(NDEBUG)
+#define JF_RELEASE
+#else
+#define JF_DEBUG
+#endif
+
+// Define static/dynamic library macros
+#if defined(JF_STATIC)
+#define JF_API
+#else
+#if defined(JF_BUILD)
+#define JF_API __declspec(dllexport)
+#else
+#define JF_API __declspec(dllimport)
+#endif
+#endif
