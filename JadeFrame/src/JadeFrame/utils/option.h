274e8ee6abc3175283a2f2dfa7fb318eda632528
Artur Marschenkulov <ArturMarschals@web.de>
2024-03-21 20:52:32 +0100

    refactor `Option` class

--- JadeFrame/src/JadeFrame/utils/option.h
+++ JadeFrame/src/JadeFrame/utils/option.h
@@ -31,47 +31,52 @@ template<typename T>
     requires std::is_lvalue_reference_v<T>
 class Storage<T> {
 public:
-    constexpr Storage()
-        : m_has_value(false) {}
+    constexpr Storage() = default;
+
     constexpr ~Storage() {
         if (m_has_value) { m_pointer = nullptr; }
     }
+
     constexpr Storage(const Storage& o)
         : m_has_value(o.m_has_value)
         , m_pointer(o.m_pointer) {}
+
     constexpr auto operator=(const Storage& o) -> Storage& {
+        if (this == &o) { return *this; }
         m_has_value = o.m_has_value;
         m_pointer = o.m_pointer;
         return *this;
     }
-    constexpr Storage(Storage&& o)
+
+    constexpr Storage(Storage&& o) noexcept
         : m_has_value(o.m_has_value)
         , m_pointer(o.m_pointer) {
         o.m_has_value = false;
         o.m_pointer = nullptr;
     }
-    constexpr auto operator=(Storage&& o) -> Storage& {
+
+    constexpr auto operator=(Storage&& o) noexcept -> Storage& {
         m_has_value = o.m_has_value;
         m_pointer = o.m_pointer;
         o.m_has_value = false;
         o.m_pointer = nullptr;
         return *this;
     }
-    constexpr Storage(const T& v)
+
+    constexpr explicit Storage(const T& v)
         : m_has_value(true)
         , m_pointer(&v) {}
-    constexpr Storage(T&& v)
+
+    constexpr explicit Storage(T&& v)
         requires(!std::is_lvalue_reference_v<T>)
         : m_has_value(true)
         , m_pointer(&v) {}
 
-
-
-    constexpr auto get() const& -> const T& { return *m_pointer; }
+    [[nodiscard]] constexpr auto get() const& -> const T& { return *m_pointer; }
 
 public:
-    std::remove_reference_t<T>* m_pointer = nullptr;
     bool                        m_has_value = false;
+    std::remove_reference_t<T>* m_pointer = nullptr;
 };
 
 template<typename T>
@@ -80,15 +85,19 @@ class Storage<T> {
 public:
     constexpr Storage()
         : m_has_value(false) {}
+
     constexpr ~Storage() {
         if (m_has_value) { reinterpret_cast<T&>(m_storage).~T(); }
     }
+
     constexpr Storage(const Storage& o)
         : m_has_value(o.m_has_value) {
-        if (o.m_has_value) { new (&m_storage) T(reinterpret_cast<const T&>(o.m_storage)); }
+        if (o.m_has_value) {
+            new (&m_storage) T(reinterpret_cast<const T&>(o.m_storage));
+        }
     }
 
-    constexpr Storage(Storage&& o)
+    constexpr Storage(Storage&& o) noexcept
         : m_has_value(o.m_has_value) {
         if (o.m_has_value) {
             T rv = std::move(reinterpret_cast<T&>(o.m_storage));
@@ -98,7 +107,7 @@ public:
         }
     }
 
-    constexpr auto operator=(Storage&& o) -> Storage& {
+    constexpr auto operator=(Storage&& o) noexcept -> Storage& {
         if (m_has_value) { reinterpret_cast<T&>(m_storage).~T(); }
         m_has_value = o.m_has_value;
         if (o.m_has_value) {
@@ -110,24 +119,24 @@ public:
         return *this;
     }
 
-    constexpr Storage(const T& v)
+    constexpr explicit Storage(const T& v)
         : m_has_value(true) {
         new (&m_storage) T(v);
     }
 
-    constexpr Storage(T&& v)
+    constexpr explicit Storage(T&& v)
         : m_has_value(true) {
         new (&m_storage) T(std::forward<T>(v));
     }
 
-    constexpr auto get() const& -> const T& {
+    [[nodiscard]] constexpr auto get() const& -> const T& {
         return reinterpret_cast<const T&>(m_storage);
     }
 
     // constexpr auto has_value() const -> bool { return m_has_value; }
 
 public:
-    alignas(T) u8 m_storage[sizeof(T)];
+    alignas(T) u8 m_storage[sizeof(T)]{};
     bool m_has_value;
 };
 } // namespace details
@@ -137,30 +146,35 @@ class Option {
 public:
     constexpr Option()
         : m_storage() {}
+
     // constexpr ~Option() {
     //     if (this->is_some()) { m_storage.get().~T(); }
     // }
     constexpr Option(const Option& o)
         : m_storage(o.m_storage) {}
+
     constexpr auto operator=(const Option& o) -> Option& {
+        if (this == &o) { return *this; }
         if (this->is_some()) { this->unwrap_unchecked().~T(); }
         m_storage = o.m_storage;
         return *this;
     }
-    constexpr Option(Option&& o)
+
+    constexpr Option(Option&& o) noexcept
         : m_storage(std::forward<Option>(o).m_storage) {}
-    constexpr auto operator=(Option&& o) -> Option& {
+
+    constexpr auto operator=(Option&& o) noexcept -> Option& {
         if (this->is_some()) { this->unwrap_unchecked().~T(); }
         m_storage = std::forward<details::Storage<T>>(o.m_storage);
         return *this;
     }
 
-    constexpr Option(const T& v)
+    constexpr explicit Option(const T& v)
         : m_storage(v) {}
-    constexpr Option(T&& v)
+
+    constexpr explicit Option(T&& v)
         requires(!std::is_lvalue_reference_v<T>)
         : m_storage(std::forward<T>(std::move(v))) {}
-
 
     constexpr auto operator==(const Option& o) const noexcept -> bool {
         if (this->is_some() && o.is_some()) {
@@ -180,9 +194,9 @@ public:
         }
     }
 
-    constexpr auto is_some() const -> bool { return m_storage.m_has_value; }
+    [[nodiscard]] constexpr auto is_some() const -> bool { return m_storage.m_has_value; }
 
-    constexpr auto is_none() const -> bool { return !this->is_some(); }
+    [[nodiscard]] constexpr auto is_none() const -> bool { return !this->is_some(); }
 
     constexpr auto unwrap() & -> T& {
         if (this->is_some()) { return m_storage.get(); }
@@ -190,7 +204,7 @@ public:
         std::terminate();
     }
 
-    constexpr auto unwrap() const& -> const T& {
+    [[nodiscard]] constexpr auto unwrap() const& -> const T& {
         if (this->is_some()) { return m_storage.get(); }
         JF_PANIC("called `Option::unwrap() const& -> const T&` on a `None` value");
         std::terminate();
@@ -200,7 +214,9 @@ public:
 
     constexpr auto unwrap_unchecked() & -> T& { return const_cast<T&>(m_storage.get()); }
 
-    constexpr auto unwrap_unchecked() const& -> const T& { return m_storage.get(); }
+    [[nodiscard]] constexpr auto unwrap_unchecked() const& -> const T& {
+        return m_storage.get();
+    }
 
     constexpr auto unwrap_unchecked() && -> T { return this->release_unchecked(); }
 
@@ -231,7 +247,7 @@ public:
     // return &std::remove_reference_t<T>(this->unwrap()); }
 
     template<typename U = T>
-    constexpr auto and_(const Option<U>& o) const& -> Option<U> {
+    [[nodiscard]] constexpr auto and_(const Option<U>& o) const& -> Option<U> {
         if (this->is_some()) {
             return o;
         } else {
@@ -239,7 +255,7 @@ public:
         }
     }
 
-    constexpr auto or_(const Option<T>& o) const& -> Option<T> {
+    [[nodiscard]] constexpr auto or_(const Option<T>& o) const& -> Option<T> {
         if (this->is_some()) {
             return *this;
         } else {
@@ -247,7 +263,7 @@ public:
         }
     }
 
-    constexpr auto xor_(const Option<T>& o) const& -> Option<T> {
+    [[nodiscard]] constexpr auto xor_(const Option<T>& o) const& -> Option<T> {
         if (this->is_some() && !o.is_some()) {
             return *this;
         } else if (!this->is_some() && o.is_some()) {
@@ -281,9 +297,9 @@ public:
 public: // C++-like wrappers
     auto value() -> T& { return this->unwrap(); }
 
-    auto value() const -> const T& { return this->unwrap(); }
+    [[nodiscard]] auto value() const -> const T& { return this->unwrap(); }
 
-    auto has_value() const -> bool { return this->is_some(); }
+    [[nodiscard]] auto has_value() const -> bool { return this->is_some(); }
 
 private:
     details::Storage<T> m_storage;
@@ -309,8 +325,8 @@ static auto test() -> void {
     }
 
     // Testing "and"
-    auto s = Option<i32>(2_i32);
-    s.and_(Option<i32>(3_i32));
+    // auto s = Option<i32>(2_i32);
+    // s.and_(Option<i32>(3_i32));
     {
         Option<i32>         x0 = Option<i32>(2_i32);
         Option<const char*> y0 = Option<const char*>();
@@ -382,8 +398,8 @@ static auto test2() -> void {
         assert(x1.is_none() == true);
     }
     // Testing "and"
-    auto s = Option<i32>(2_i32);
-    s.and_(Option<i32>(3_i32));
+    // auto s = Option<i32>(2_i32);
+    // s.and_(Option<i32>(3_i32));
 
     {
         Option<i32>         x0 = Option<i32>(2_i32);
@@ -428,7 +444,7 @@ static auto lvalue_ref() -> void {
         int         age;
         std::string name;
 
-        auto get_name() const -> Option<const std::string&> {
+        [[nodiscard]] auto get_name() const -> Option<const std::string&> {
             if (age >= 18) {
                 return Option<const std::string&>(name);
             } else {
