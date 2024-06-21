38aed498d48fa1fff65ab7532e8dcfea063ee2b4
Artur Marschenkulov <ArturMarschals@web.de>
2024-06-21 23:00:11 +0200

    update various meta files like CMakeLists, clang-tidy etc

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+build_dir="build"
+
+
+# if clean is passed as an argument, remove the build directory
+if [ "$1" == "clean" ]; then
+    if [ -d $build_dir ]; then
+        rm -r $build_dir
+    fi
+    exit 0
+fi
+
+if [ ! -d $build_dir ]; then
+    mkdir $build_dir
+fi
+cd $build_dir
+
+# cmake ../. && cmake --build . -j10 && ./Application/Application
+
+if cmake ../. && cmake --build . -j10; then
+    echo "Build successful"
+    compiledb -n make
+    echo "Compiledb generated"
+    echo "Running Application"
+    ./Application/Application
+else
+    echo "Build failed"
+fi
