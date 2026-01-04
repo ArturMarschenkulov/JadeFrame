34ea2b2f75db28c49dd8c16636489d2520b2138e
Artur Marschenkulov <ArturMarschals@web.de>
2026-01-04 21:26:43 +0100

    refactor(logger): remove unused wide character log functions and streamline log level conversion

--- JadeFrame/src/JadeFrame/utils/logger.h
+++ JadeFrame/src/JadeFrame/utils/logger.h
@@ -50,31 +50,28 @@ public:
     template<class... Types>
     static auto critical(fmt::format_string<Types...> text, Types&&... args) -> void;
 
-    // template<class... Types>
-    // static auto log(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto log(LEVEL level, fmt::wformat_string<Types...> text, Types&&... args)
-    // -> void;
-
-    // template<class... Types>
-    // static auto trace(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto debug(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto info(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto warn(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto err(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-    // template<class... Types>
-    // static auto critical(fmt::wformat_string<Types...> text, Types&&... args) -> void;
-
     static auto init() -> void;
     static auto deinit() -> void;
 
     static std::shared_ptr<spdlog::logger> s_core;
     static std::shared_ptr<spdlog::logger> s_client;
     static std::shared_ptr<spdlog::logger> s_editor;
+
+private:
+    static constexpr auto to_spd(Logger::LEVEL level) noexcept
+        -> spdlog::level::level_enum {
+        using enum Logger::LEVEL;
+        switch (level) {
+            case TRACE: return spdlog::level::trace;
+            case DEBUG: return spdlog::level::debug;
+            case INFO: return spdlog::level::info;
+            case WARN: return spdlog::level::warn;
+            case ERR: return spdlog::level::err;
+            case CRITICAL: return spdlog::level::critical;
+            case OFF: return spdlog::level::off;
+        }
+        return spdlog::level::off; // defensive
+    }
 };
 
 template<class... Types>
@@ -85,16 +82,7 @@ auto Logger::log(fmt::format_string<Types...> text, Types&&... args) -> void {
 template<class... Types>
 auto Logger::log(LEVEL level, fmt::format_string<Types...> text, Types&&... args)
     -> void {
-    spdlog::level::level_enum lvl;
-    switch (level) {
-        case LEVEL::TRACE: lvl = spdlog::level::trace; break;
-        case LEVEL::DEBUG: lvl = spdlog::level::debug; break;
-        case LEVEL::INFO: lvl = spdlog::level::info; break;
-        case LEVEL::WARN: lvl = spdlog::level::warn; break;
-        case LEVEL::ERR: lvl = spdlog::level::err; break;
-        case LEVEL::CRITICAL: lvl = spdlog::level::critical; break;
-        default: lvl = spdlog::level::off; break;
-    }
+    spdlog::level::level_enum lvl = Logger::to_spd(level);
     // if (sizeof...(args) > 1) {
     //     s_core->log(lvl, text, std::forward<Types>(args)...);
     // } else {
@@ -133,52 +121,4 @@ auto Logger::critical(fmt::format_string<Types...> text, Types&&... args) -> voi
     Logger::log(LEVEL::CRITICAL, text, std::forward<Types>(args)...);
 }
 
-// // Wide char version
-
-// template<class... Types>
-// auto Logger::log(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
-// }
-
-// template<class... Types>
-// auto Logger::log(LEVEL level, fmt::wformat_string<Types...> text, Types&&... args) ->
-// void {
-//     spdlog::level::level_enum lvl;
-//     switch (level) {
-//         case LEVEL::TRACE: lvl = spdlog::level::trace; break;
-//         case LEVEL::DEBUG: lvl = spdlog::level::debug; break;
-//         case LEVEL::INFO: lvl = spdlog::level::info; break;
-//         case LEVEL::WARN: lvl = spdlog::level::warn; break;
-//         case LEVEL::ERR: lvl = spdlog::level::err; break;
-//         case LEVEL::CRITICAL: lvl = spdlog::level::critical; break;
-//         default: lvl = spdlog::level::off; break;
-//     }
-
-//     s_core->log(lvl, text, std::forward<Types>(args)...);
-// }
-
-// template<class... Types>
-// auto Logger::trace(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::TRACE, text, std::forward<Types>(args)...);
-// }
-// template<class... Types>
-// auto Logger::debug(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::DEBUG, text, std::forward<Types>(args)...);
-// }
-// template<class... Types>
-// auto Logger::info(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::INFO, text, std::forward<Types>(args)...);
-// }
-// template<class... Types>
-// auto Logger::warn(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::WARN, text, std::forward<Types>(args)...);
-// }
-// template<class... Types>
-// auto Logger::err(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::ERR, text, std::forward<Types>(args)...);
-// }
-// template<class... Types>
-// auto Logger::critical(fmt::wformat_string<Types...> text, Types&&... args) -> void {
-//     Logger::log(LEVEL::CRITICAL, text, std::forward<Types>(args)...);
-// }
 } // namespace JadeFrame
