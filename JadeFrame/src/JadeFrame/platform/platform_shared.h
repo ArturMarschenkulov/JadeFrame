8776588f3701c11a5a074656937753edd95fe0b7
Artur Marschenkulov <ArturMarschals@web.de>
2026-01-04 21:29:41 +0100

    refactor(platform): move platform-specific functions to platform_shared.cpp for better organization

--- JadeFrame/src/JadeFrame/platform/platform_shared.h
+++ JadeFrame/src/JadeFrame/platform/platform_shared.h
@@ -121,7 +121,7 @@ consteval auto get_architecture_info() -> const char* {
     name = "ARM";
 #elif defined(__aarch64__) || defined(_M_ARM64)
     name = "ARM64";
-#elif
+#else
     name = "UNKNOWN";
 #endif
     return name;
@@ -165,63 +165,7 @@ public:
     virtual auto               set_FPS(f64 FPS) -> void = 0;
 };
 
-#if defined(JF_PLATFORM_LINUX)
-    #include <dlfcn.h>
-#endif
-
-static auto get_program_path() -> std::string {
-#if defined(JF_PLATFORM_LINUX)
-    char    buf[1024] = {0};
-    ssize_t buf_size = readlink("/proc/self/exe", buf, sizeof(buf) /*- 1*/);
-
-    if (buf_size == -1) {
-        auto  error_code = errno;
-        char* error_str = std::strerror(error_code);
-        Logger::err(
-            "readlink(/proc/self/exe) failed with error code: {}: {}",
-            error_code,
-            error_str
-        );
-    }
-    std::string path = buf;
-    std::size_t path_end = path.find_last_of('/');
-    if (path_end != std::string::npos) { path.resize(path_end + 1); }
-    return path;
-#elif defined(JF_PLATFORM_WINDOWS)
-    return "";
-#endif
-}
-
-static auto load_module(const char* path) -> void* {
-#if defined(JF_PLATFORM_LINUX)
-    return dlopen(path, RTLD_LAZY | RTLD_LOCAL);
-#elif defined(JF_PLATFORM_WINDOWS)
-    return LoadLibraryA(path);
-#else
-    JF_UNIMPLEMENTED("");
-#endif
-}
-#if defined(JF_PLATFORM_LINUX)
-    #include <sys/mman.h>
-#endif
-
-static auto alloc_page(u64 page_amount) -> void* {
-    constexpr u64 page_size = 4096;
-#if defined(JF_PLATFORM_LINUX)
-    return mmap(
-        nullptr,
-        page_amount * page_size,
-        PROT_READ | PROT_WRITE,
-        MAP_PRIVATE | MAP_ANONYMOUS,
-        -1,
-        0
-    );
-#elif defined(JF_PLATFORM_WINDOWS)
-    return VirtualAlloc(
-        nullptr, page_amount * page_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE
-    );
-#else
-    JF_UNIMPLEMENTED("");
-#endif
-}
+auto get_program_path() -> std::string;
+auto load_module(const char* path) -> void*;
+auto alloc_page(u64 page_amount) -> void*;
 } // namespace JadeFrame
